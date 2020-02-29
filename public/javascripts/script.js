e2a706ee68694189b1e16661dad3dca2e35e91aa
Humberto Buniotto <humbertobuniotto@Humbertos-MacBook-Pro.local>
2020-02-28 22:11:02 -0500

    Finished News, Profile & Profile Edit pages

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+$(window).on('load',function(){ // opens the interest modal on window load
+  $('#interestModal').modal('show');
+});
