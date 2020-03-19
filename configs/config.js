d150677a8534ef625a7f54c917d42192ef005314
Erick Sicard <esrfree@gmail.com>
2020-03-18 23:23:03 -0400

    Heroku database connection string

--- configs/config.js
+++ configs/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI || 'mongodb://localhost/ironhack-flow-project'
+  mongoUri: process.env.MONGODB_URI || 'mongodb://heroku_48mjcgj8:9gfd8700vpn5t0n0sboc1gi37n@ds151018.mlab.com:51018/heroku_48mjcgj8'
   
 }
 
